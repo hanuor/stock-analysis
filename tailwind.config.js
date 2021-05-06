69ff5dd929a3fe79a661f3098dc74cb02db99297
kristjanmar <kriistjanm@gmail.com>
2021-05-06 19:12:11 +0700

    Fix for missing tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-	purge: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}'
-	],
+	purge: {
+		content: [
+			'./pages/**/*.{js,ts,jsx,tsx}',
+			'./components/**/*.{js,ts,jsx,tsx}'
+		],
+		options: {
+			safelist: ['border-gray-300', 'bg-blue-400']
+		}
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		container: {
