da1c2614d918c33e243efb1f06c2bdc2ddc46550
kristjanmar <kriistjanm@gmail.com>
2021-05-08 19:20:14 +0700

    Added to tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,27 @@
 module.exports = {
 	purge: {
 		content: [
-			'./pages/**/*.{js,ts,jsx,tsx}',
-			'./components/**/*.{js,ts,jsx,tsx}'
+			"./pages/**/*.{js,ts,jsx,tsx}",
+			"./components/**/*.{js,ts,jsx,tsx}",
 		],
 		options: {
-			safelist: ['border-gray-300', 'bg-blue-400']
-		}
+			safelist: ["border-gray-300", "bg-blue-400", "max-w-md", "my-20"],
+		},
 	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		container: {
 			center: true,
-			padding: "1.5rem"
+			padding: "1.5rem",
 		},
 		extend: {
 			gridTemplateColumns: {
-				"sidebar": "1fr 300px"
-			}
+				sidebar: "1fr 300px",
+			},
 		},
 	},
 	variants: {
 		extend: {},
 	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-}
+	plugins: [require("@tailwindcss/typography")],
+};
