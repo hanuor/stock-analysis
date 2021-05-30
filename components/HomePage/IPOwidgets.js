5bfa1ece2329ab96f04e76107bbf89925fa991c9
Kris <kriistjanm@gmail.com>
2021-05-30 12:59:52 +0700

    Styling fix

--- components/HomePage/IPOwidgets.js
+++ components/HomePage/IPOwidgets.js
@@ -21,7 +21,7 @@ export default function IPOwidgets({ recent, upcoming }) {
 							<tr
 								key={index}
 								className="border-b border-gray-200 hover:bg-gray-50">
-								<td className="py-1.5 px-2 border-r border-gray-200">
+								<td className="py-1.5 px-2 border-r border-gray-200 whitespace-nowrap">
 									{item.d}
 								</td>
 								<td className="py-1.5 px-2 border-r border-gray-200">
