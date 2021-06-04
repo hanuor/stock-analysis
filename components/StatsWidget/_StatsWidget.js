4087578011639cf9cf7a239e505c1fc718219d31
Kris <kriistjanm@gmail.com>
2021-06-04 18:25:54 +0700

    Added all the data for the statistics pages

--- components/StatsWidget/_StatsWidget.js
+++ components/StatsWidget/_StatsWidget.js
@@ -1,22 +1,10 @@
 import styles from './StatsWidget.module.css';
 import Tooltip from './StatsTooltips';
 
-const formatNumber = (number, type) => {
-	switch (type) {
-		case 'standard':
-			return new Intl.NumberFormat('en-US', {
-				maximumFractionDigits: 2,
-			}).format(number);
-
-		default:
-			return number;
-	}
-};
-
 const StatsWidget = ({ title, text, data, map }) => (
 	<>
 		<h2 className="text-xl font-bold mb-1.5">{title}</h2>
-		<p className="text-[1.05rem] mb-3">{text}</p>
+		<p className="text-base mb-3">{text}</p>
 		<table className={styles.statstable}>
 			<tbody>
 				{data.map((item) => {
@@ -27,7 +15,7 @@ const StatsWidget = ({ title, text, data, map }) => (
 							<td>
 								<Tooltip data={indicator_info} indicator={item[1]} />
 							</td>
-							<td title={formatNumber(item[3], item[4])}>{item[2]}</td>
+							<td title={item[3]}>{item[2]}</td>
 						</tr>
 					);
 				})}
--- pages/stocks/[symbol]/statistics.js
+++ pages/stocks/[symbol]/statistics.js
@@ -18,7 +18,7 @@ export default function SymbolStatistics({ info, data }) {
 
 	return (
 		<Stock>
-			<div className="px-4 lg:px-6 mx-auto md:grid md:grid-cols-3">
+			<div className="px-4 lg:px-6 mx-auto md:grid md:grid-cols-3 md:gap-8">
 				<div>
 					<StatsWidget
 						title="Total Valuation"
@@ -45,6 +45,94 @@ export default function SymbolStatistics({ info, data }) {
 						title="Valuation Ratios"
 						text={data.ratios.text}
 						data={data.ratios.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Enterprise Valuation"
+						text={data.evratios.text}
+						data={data.evratios.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Financial Position"
+						text={data.financialPosition.text}
+						data={data.financialPosition.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Financial Efficiency"
+						text={data.financialEfficiency.text}
+						data={data.financialEfficiency.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Taxes"
+						text={data.taxes.text}
+						data={data.taxes.data}
+						map={indicator_map}
+					/>
+				</div>
+
+				<div>
+					<StatsWidget
+						title="Stock Price Statistics"
+						text={data.stockprice.text}
+						data={data.stockprice.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Short Selling Information"
+						text={data.short.text}
+						data={data.short.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Income Statement"
+						text={data.income.text}
+						data={data.income.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Balance Sheet"
+						text={data.balance.text}
+						data={data.balance.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Cash Flow"
+						text={data.cashflow.text}
+						data={data.cashflow.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Margins"
+						text={data.margins.text}
+						data={data.margins.data}
+						map={indicator_map}
+					/>
+				</div>
+
+				<div>
+					<StatsWidget
+						title="Dividends & Yields"
+						text={data.dividends.text}
+						data={data.dividends.data}
+						map={indicator_map}
+					/>
+
+					<StatsWidget
+						title="Stock Splits"
+						text={data.splits.text}
+						data={data.splits.data}
 						map={indicator_map}
 					/>
 				</div>
