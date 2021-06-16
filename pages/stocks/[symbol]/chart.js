6423b21ba7169767b5fcb636b0331cc29a8366a4
Kris <kriistjanm@gmail.com>
2021-06-16 21:04:27 +0700

    Fix eslint error

--- pages/stocks/[symbol]/chart.js
+++ pages/stocks/[symbol]/chart.js
@@ -1,37 +1,37 @@
-import Stock from "@/components/Layout/StockLayout";
-import { getPageData, getStockInfo } from "@/Functions/fetchStockInfo";
-import { stockState } from "@State/stockState";
-import { useEffect } from "react";
+import Stock from '@/components/Layout/StockLayout';
+import { getPageData, getStockInfo } from '@/Functions/fetchStockInfo';
+import { stockState } from '@State/stockState';
+import { useEffect } from 'react';
 
-export default function SymbolStatistics(props) {
-	if (!props.info) {
-		return <h1>Loading...</h1>;
-	}
-
+export default function SymbolStatistics({ info, data }) {
 	const setInfo = stockState((state) => state.setInfo);
 	const setData = stockState((state) => state.setData);
 
 	useEffect(() => {
-		setInfo(props.info);
-		setData(props.data);
-	}, []);
+		setInfo(info);
+		setData(data);
+	}, [data, info, setData, setInfo]);
+
+	if (!info) {
+		return null;
+	}
 
 	return (
 		<Stock>
 			<h2 className="text-2xl font-bold my-8">
-				This is the chart page for {props.info.ticker}
+				This is the chart page for {info.ticker}
 			</h2>
 		</Stock>
 	);
 }
 
 export async function getStaticPaths() {
-	return { paths: [], fallback: "blocking" };
+	return { paths: [], fallback: 'blocking' };
 }
 
 export async function getStaticProps({ params }) {
 	const info = await getStockInfo({ params });
-	const data = await getPageData(info.id, "overview");
+	const data = await getPageData(info.id, 'overview');
 
 	return {
 		props: {
