10490bde16d19f0ff653628cb18f7622e296832c
Kristjan <kriistjanm@gmail.com>
2021-06-20 07:03:55 +0000

    Add ETF holdings pages

---
+++ components/Holdings/_HoldingsTable.js
@@ -0,0 +1,125 @@
+import { useMemo } from 'react';
+import { useTable, usePagination } from 'react-table';
+import Pagination from '@/components/Tables/Pagination';
+import styles from './HoldingsTable.module.css';
+import Paywall from './HoldingsPaywall';
+import StockLink, { ETFLink } from '@/components/Links';
+import userState from '@State/userState';
+
+const _HoldingsTable = ({ rawdata }) => {
+	const isLoggedIn = userState((state) => state.isLoggedIn);
+
+	const columns = useMemo(
+		() => [
+			{
+				Header: 'No.',
+				accessor: 'no',
+			},
+			{
+				Header: 'Symbol',
+				accessor: 'symbol',
+				Cell: function FormatCell({ cell: { value } }) {
+					if (value.startsWith('$')) {
+						return <StockLink symbol={value.slice(1)} />;
+					} else if (value.startsWith('#')) {
+						return <ETFLink symbol={value.slice(1)} />;
+					}
+					return value;
+				},
+			},
+			{
+				Header: 'Company Name',
+				accessor: 'name',
+			},
+			{
+				Header: '% Assets',
+				accessor: 'assets',
+			},
+			{
+				Header: 'Shares',
+				accessor: 'shares',
+			},
+		],
+		[]
+	);
+
+	const data = useMemo(() => rawdata, [rawdata]);
+
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		prepareRow,
+		rows,
+		page,
+		canPreviousPage,
+		canNextPage,
+		pageOptions,
+		nextPage,
+		previousPage,
+		setPageSize,
+		state: { pageIndex, pageSize },
+	} = useTable(
+		{
+			columns,
+			data,
+			initialState: {
+				pageIndex: 0,
+				pageSize: 200,
+			},
+		},
+		usePagination
+	);
+
+	return (
+		<>
+			<div className="overflow-x-auto">
+				<table {...getTableProps()} className={styles.table}>
+					<thead>
+						{headerGroups.map((headerGroup) => (
+							<tr {...headerGroup.getHeaderGroupProps()}>
+								{headerGroup.headers.map((column) => (
+									<th {...column.getHeaderProps()}>
+										{column.render('Header')}
+									</th>
+								))}
+							</tr>
+						))}
+					</thead>
+					<tbody {...getTableBodyProps()}>
+						{page.map((row) => {
+							prepareRow(row);
+							return (
+								<tr {...row.getRowProps()}>
+									{row.cells.map((cell) => {
+										return (
+											<td {...cell.getCellProps()}>
+												{cell.render('Cell')}
+											</td>
+										);
+									})}
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+			{isLoggedIn && rows.length > 200 ? (
+				<Pagination
+					previousPage={previousPage}
+					canPreviousPage={canPreviousPage}
+					pageIndex={pageIndex}
+					pageOptions={pageOptions}
+					pageSize={pageSize}
+					setPageSize={setPageSize}
+					nextPage={nextPage}
+					canNextPage={canNextPage}
+				/>
+			) : (
+				<Paywall total={rows.length} />
+			)}
+		</>
+	);
+};
+
+export default _HoldingsTable;
---
+++ pages/etf/[symbol]/holdings.js
@@ -0,0 +1,80 @@
+import Stock from '@/components/Layout/StockLayout';
+import { getPageData, getEtfInfo } from '@/Functions/callBackEnd';
+import { stockState } from '@State/stockState';
+import { useEffect } from 'react';
+import HoldingsTable from '@/components/Holdings/_HoldingsTable';
+import NewsWidget from '@/components/News/NewsWidget';
+
+export default function Holdings({ info, data }) {
+	const setInfo = stockState((state) => state.setInfo);
+	const setData = stockState((state) => state.setData);
+
+	useEffect(() => {
+		setInfo(info);
+		setData(data);
+	}, [data, info, setData, setInfo]);
+
+	const HeaderFull = () => (
+		<>
+			{info.ticker} Holdings: {data.data.count}
+		</>
+	);
+
+	const HeaderEmpty = () => <>{info.ticker} Holdings</>;
+
+	return (
+		<Stock>
+			<div className="contain">
+				<div className="lg:grid grid-cols-sidebar_wide gap-8">
+					<div>
+						<div className="flex flex-row justify-between items-end mb-1">
+							<h1 className="text-xl bp:text-2xl font-gray-900 font-bold mt-1 mb-0.5 bp:mb-1 sm:mb-2">
+								{data.data.count ? <HeaderFull /> : <HeaderEmpty />}
+							</h1>
+
+							{data.data.count && (
+								<span className="text-gray-700 text-small bp:text-smaller">
+									Updated {data.data.updated}
+								</span>
+							)}
+						</div>
+						{data.data.count ? (
+							<HoldingsTable rawdata={data.data.list} />
+						) : (
+							<div className="text-lg">
+								No holdings were found for the {info.ticker} ETF
+							</div>
+						)}
+					</div>
+					<aside className="mt-7 lg:mt-5">
+						<NewsWidget
+							title={`${info.ticker} News`}
+							news={data.news}
+							button={{
+								text: 'More News',
+								url: `/etf/${info.symbol}/`,
+							}}
+						/>
+					</aside>
+				</div>
+			</div>
+		</Stock>
+	);
+}
+
+export async function getStaticProps({ params }) {
+	const info = await getEtfInfo({ params });
+	const data = await getPageData(info.id, 'holdings');
+
+	return {
+		props: {
+			info,
+			data,
+		},
+		revalidate: 300,
+	};
+}
+
+export async function getStaticPaths() {
+	return { paths: [], fallback: 'blocking' };
+}
