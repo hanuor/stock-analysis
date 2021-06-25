887c2b36258630c3c30c71b2269abe65f0f48cb3
azidn1nja <azidspider@hotmail.com>
2021-06-25 13:47:56 +0000

    Quick chart api fix

--- pages/api/chart.js
+++ pages/api/chart.js
@@ -4,23 +4,22 @@ export default async (req, res) => {
 	if (req.method === 'GET') {
 		const id = req.query.i;
 		const minimal = req.query.m ? true : false;
-		var period = req.query.p;
-		var time = req.query.t;
+		let period = req.query.p;
+		let time = req.query.t;
 
 		if (!period) {
-			period = "";
+			period = '';
 		}
 		if (!time) {
-			time = "";
+			time = '';
 		}
 		let API =
 			process.env.API_CHARTS || 'https://stockanalysis.com/wp-json/sa/cch';
-		const url = API + `/?i=${id}${minimal && '&m=1'}`;
+		//const url = API + `/?i=${id}${minimal && '&m=1'}`;
 
-		const url = `${process.env.API_CHARTS}?i=${id}${time && "&r=" + time}${
-			period && "&p=" + period
-		}`;
-		console.log(url);
+		const url =
+			API + `/?i=${id}${time && '&r=' + time}${period && '&p=' + period}`;
+
 		await axios
 			.get(url)
 			.then((response) => {
