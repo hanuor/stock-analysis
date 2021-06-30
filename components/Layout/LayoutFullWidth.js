3ad50ae00db1007f5a269a4507a2a7e6c8ad1fef
Kristjan <kriistjanm@gmail.com>
2021-06-30 11:27:47 +0000

    Move Header and Footer into _app.js

--- components/Layout/LayoutFullWidth.js
+++ components/Layout/LayoutFullWidth.js
@@ -1,15 +1,11 @@
 import Meta from '@/components/Meta';
-import Header from '@/components/Layout/Header/_Header';
-import Footer from '@/components/Layout/Footer/_Footer';
 
 function LayoutFullWidth(props) {
 	return (
 		<>
 			<Meta title={props.title}></Meta>
 			<div className="">
-				<Header />
 				<main>{props.children}</main>
-				<Footer />
 			</div>
 		</>
 	);
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
+import Header from '@/Layout/Header/_Header';
+import Footer from '@/Layout/Footer/_Footer';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Header />
+			<Component {...pageProps} />
+			<Footer />
+		</>
+	);
 }
 
 export default MyApp;
