72999338b4f34fa9b42386a86837e0db19620fbd
Kris Gunnars <kriistjanm@gmail.com>
2021-07-02 12:20:50 +0100

    Installed eslint, prettier, changed _app to .tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-// Move main to typescript
+import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 /* import { ReactQueryDevtools } from 'react-query/devtools' */
@@ -8,15 +8,15 @@ import Footer from 'components/Layout/Footer/_Footer';
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }) {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<Header />
-			<Component {...pageProps} />
-			<Footer />
-			{/* <ReactQueryDevtools initialIsOpen={false} /> */}
-		</QueryClientProvider>
-	);
+function MyApp({ Component, pageProps }: AppProps) {
+   return (
+      <QueryClientProvider client={queryClient}>
+         <Header />
+         <Component {...pageProps} />
+         <Footer />
+         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </QueryClientProvider>
+   );
 }
 
 export default MyApp;
