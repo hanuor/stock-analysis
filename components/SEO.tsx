f3f811e41fe82dae68dd2d5a85dbd58f0fc5515b
Kris Gunnars <kriistjanm@gmail.com>
2021-07-03 20:27:44 +0000

    Added to SEO component

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -5,6 +5,7 @@ interface ISEO {
 	description?: string;
 	canonical: string;
 	noindex?: boolean;
+	image?: string;
 }
 
 export const SEO = ({
@@ -12,25 +13,44 @@ export const SEO = ({
 	description,
 	canonical,
 	noindex = false,
+	image,
 }: ISEO) => {
+	const seoTitle =
+		title === 'Stock Analysis | Free Online Stock Information for Investors'
+			? title
+			: title + ' | Stock Analysis';
+
+	const metaRobots = !noindex
+		? 'max-snippet:-1,max-image-preview:large,max-video-preview:-1'
+		: 'noindex, nofollow';
+
+	const canonicalUrl = 'https://stockanalysis.com/' + canonical;
+
+	const featuredImage = image || 'https://stockanalysis.com/bear-vs-bull.jpg';
+
 	return (
 		<Head>
-			<title>{title}</title>
+			<title>{seoTitle}</title>
 			{description && <meta name="description" content={description} />}
 
-			<meta
-				name="robots"
-				content={
-					!noindex
-						? 'max-snippet:-1,max-image-preview:large,max-video-preview:-1'
-						: 'noindex, nofollow'
-				}
-			/>
+			<meta name="robots" content={metaRobots} />
+			<link rel="canonical" href={canonicalUrl} />
 
-			<link
-				rel="canonical"
-				href={'https://stockanalysis.com/' + canonical}
-			/>
+			<meta property="og:image" content={featuredImage} />
+			<meta property="og:title" content={seoTitle} />
+			{description && (
+				<meta property="og:description" content={description} />
+			)}
+			<meta property="og:url" content={canonicalUrl} />
+			<meta property="og:site_name" content="Stock Analysis" />
+
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="@stock_analysisx" />
+			<meta name="twitter:title" content={seoTitle} />
+			{description && (
+				<meta name="twitter:description" content={description} />
+			)}
+			<meta name="twitter:image" content={featuredImage} />
 		</Head>
 	);
 };
