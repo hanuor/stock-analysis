7f37861dd9e8f86be8d4e3d11c4f1e0a7cb8123b
Kristjan <kriistjanm@gmail.com>
2021-07-15 16:06:05 +0000

    Move react -query client provider to wrap stock price instead of the entire app

--- components/StockHeading/_StockHeading.tsx
+++ components/StockHeading/_StockHeading.tsx
@@ -2,15 +2,24 @@ import Title from 'components/StockHeading/Title';
 import StockPrice from 'components/StockHeading/StockPrice';
 import TabNavigation from 'components/StockHeading/TabNavigation';
 import ETFNavigation from 'components/StockHeading/TabNavigationETF';
-
 import Breadcrumbs from 'components/Breadcrumbs/_Breadcrumbs';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-export default function StockHeading({ type, id }) {
+const queryClient = new QueryClient();
+
+interface Props {
+	type: string;
+	id: number;
+}
+
+export default function StockHeading({ type, id }: Props) {
 	return (
 		<div className="mx-auto px-3 xs:px-4 lg:px-6 mb-4 sm:mb-5">
 			<Breadcrumbs />
 			<Title />
-			<StockPrice id={id} />
+			<QueryClientProvider client={queryClient}>
+				<StockPrice id={id} />
+			</QueryClientProvider>
 			{type === 'etf' ? <ETFNavigation /> : <TabNavigation />}
 		</div>
 	);
