1ed3ef1e94180767730fdac6bfc72fed1a312ec2
Kris Gunnars <kriistjanm@gmail.com>
2021-07-21 17:36:46 +0000

    Add transition to search dropdown

--- components/StockHeading/StockPrice.tsx
+++ components/StockHeading/StockPrice.tsx
@@ -91,16 +91,16 @@ function changeColor(change?: number) {
 // Regular price if market open or no extended price available
 function Regular({ quote }: { quote: Quote }) {
 	const color = changeColor(quote.changeR);
+	const market = quote.market == 'open' ? 'Market open' : 'Market closed';
 
 	return (
 		<div>
 			<span className="text-4xl font-bold">{quote.priceD}</span>{' '}
 			<span className={`text-2xl ${color} font-semibold`}>
-				{quote.change} ({quote.changePc})
+				{`${quote.change} (${quote.changePc})`}
 			</span>
 			<div className="text-sm text-gray-700 flex items-center mt-1">
-				{quote.timestamp} -{' '}
-				{quote.market == 'open' ? <>Market open</> : <>Market closed</>}
+				{`${quote.timestamp} - ${market}`}
 			</div>
 		</div>
 	);
