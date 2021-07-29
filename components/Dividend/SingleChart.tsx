f5ce8a20cb772675ff73fb2e5eadb8db563678f1
azidn1nja <azidspider@hotmail.com>
2021-07-29 01:32:10 +0000

    Tag added for Dividends

--- components/Dividend/SingleChart.tsx
+++ components/Dividend/SingleChart.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { formatDateClean } from 'functions/formatDates';
+import { format } from 'd3-format';
 
 const countZero = (cutter: number[]) => {
 	let count = 0;
@@ -40,7 +41,91 @@ export const SingleChart = ({ xdata, ydata, type, title }: Props) => {
 						},
 					],
 				}}
+				plugins={[
+					{
+						afterDatasetsDraw: function (chart: any) {
+							const chartInstance = chart;
+							const ctx = chartInstance.ctx;
+							ctx.font =
+								'13px -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+							ctx.textAlign = 'start';
+							ctx.textBaseline = 'bottom';
+
+							chartInstance.data.datasets.forEach(function (
+								dataset: { data: any[] },
+								i: any
+							) {
+								const meta = chartInstance.getDatasetMeta(i);
+
+								const last = meta.data.length - 1; // The last index of the array, so that the latest stock price is shown
+								if (meta.data.length == 0 || dataset.data[last] == 0)
+									return;
+
+								// numericals are offsets for positional purposes, x and y marks the exact coordinates of the graph end.
+
+								const x =
+									meta.vScale._labelItems[
+										meta.vScale._labelItems.length - 1
+									].translation[0] - 0.5;
+
+								const y = meta.data[last].y - 9;
+								let str = dataset.data[last];
+								// retrieve the stock price, data.
+
+								if (type == 'percentage') {
+									str = format('.2%')(Number(str) / 100); // rounded percentage, "12%"
+								} else {
+									if (Number(str) >= 1) {
+										str = format('.2~f')(str);
+									} else if (Number(str) < 0.1) {
+										str = format('.4~f')(str);
+									} else if (Number(str) < 1) {
+										str = format('.3~f')(str);
+									}
+									str = '$' + str;
+								}
+
+								// const str = dataset.data[last];
+
+								// begin drawing and styling
+
+								ctx.save();
+
+								ctx.strokeStyle = '#2c6288';
+								ctx.fillStyle = '#2c6288';
+								ctx.lineWidth = '3.5';
+								ctx.lineJoin = 'round';
+
+								// calculate the width of the box and height is based on fontsize.
+								const width = ctx.measureText(str).width + 1.2;
+								const height = 14.8;
+
+								// draw triangle to form a pointer.
+								ctx.beginPath();
+								ctx.moveTo(x - 7.7, y + 1.5 + height / 2);
+								ctx.lineTo(x + 0.7, y + 2.5 + height);
+								ctx.lineTo(x + 0.7, y + 0.5);
+								ctx.fill();
+								ctx.closePath();
+
+								// draw the box
+								ctx.strokeRect(x + 2, y + 1.5, width, height);
+								ctx.fillRect(x + 2, y + 1.5, width, height);
+
+								// draw the text
+								ctx.fillStyle = '#ffffff';
+								ctx.fillText(str, x + 1.5, y + 16.5);
+								ctx.restore();
+							});
+						},
+					},
+				]}
 				options={{
+					layout: {
+						padding: {
+							right: 20,
+						},
+					},
 					maintainAspectRatio: false,
 					scales: {
 						x: {
@@ -89,7 +174,7 @@ export const SingleChart = ({ xdata, ydata, type, title }: Props) => {
 							color: '#333',
 							padding: {
 								top: 10,
-								right: 5,
+								right: 40,
 								bottom: 12,
 								left: 5,
 							},
