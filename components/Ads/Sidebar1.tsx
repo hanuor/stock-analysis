7dc66c005f21719092ede17b03569b1af1feb3cd
Kristjan <kriistjanm@gmail.com>
2021-08-09 11:06:47 +0700

    Working on ads

--- components/Ads/Sidebar1.tsx
+++ components/Ads/Sidebar1.tsx
@@ -16,7 +16,12 @@ export const Sidebar1 = () => {
 		<>
 			{status === 'completed' && !isPro && (
 				<>
-					<div id="div-gpt-ad-1617185412139-0"></div>
+					<div className="lbl mt-4 mb-1">
+						<div
+							className="min-h-[250px] w-[300px] bg-gray-50 mx-auto"
+							id="div-gpt-ad-1617185412139-0"
+						></div>
+					</div>
 					<Script
 						strategy="lazyOnload"
 						dangerouslySetInnerHTML={{
