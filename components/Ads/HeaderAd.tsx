d42e8e931cd6ec18baf5130d89588e0ca8413b43
Kristjan <kriistjanm@gmail.com>
2021-08-09 18:25:24 +0700

    GPT

--- components/Ads/HeaderAd.tsx
+++ components/Ads/HeaderAd.tsx
@@ -1,23 +1,5 @@
 import { useAd } from 'hooks/useAd';
 
-// const headerAd = (googletag: any) => {
-// 	const mappingB = googletag
-// 		.sizeMapping()
-// 		.addSize([768, 0], [728, 90])
-// 		.addSize([0, 0], [])
-// 		.build();
-
-// 	// Sidebar 1 -- slot
-// 	googletag
-// 		.defineSlot(
-// 			'/2507246/SAN//stockanalysis//misc//2',
-// 			[728, 90],
-// 			'div-gpt-ad-1617185422059-0'
-// 		)
-// 		.defineSizeMapping(mappingB)
-// 		.addService(googletag.pubads());
-// };
-
 export const HeaderAd = () => {
 	useAd('header');
 
--- components/Ads/Sidebar1.tsx
+++ components/Ads/Sidebar1.tsx
@@ -1,36 +1,12 @@
 import { useAd } from 'hooks/useAd';
 
-// const sidebar1 = (googletag: any) => {
-// 	const mappingA = googletag
-// 		.sizeMapping()
-// 		.addSize(
-// 			[1024, 0],
-// 			[
-// 				[336, 280],
-// 				[300, 250],
-// 			]
-// 		)
-// 		.addSize([0, 0], [])
-// 		.build();
-
-// 	// Sidebar 1 -- slot
-// 	googletag
-// 		.defineSlot(
-// 			'/2507246/SAN//stockanalysis//misc//1',
-// 			[300, 250],
-// 			'div-gpt-ad-1617185412139-0'
-// 		)
-// 		.defineSizeMapping(mappingA)
-// 		.addService(googletag.pubads());
-// };
-
 export const Sidebar1 = () => {
 	useAd('sidebar1');
 
 	return (
 		<>
 			<div
-				className="min-h-[250px] min-w-[300px] bg-gray-50 mx-auto"
+				className="min-h-[250px] max-h-[280px] min-w-[300px] bg-gray-50 mx-auto"
 				id="div-gpt-ad-1617185412139-0"
 			></div>
 		</>
