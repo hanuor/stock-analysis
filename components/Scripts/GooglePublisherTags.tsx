46bbc869dfeb49ea7402aeab0cce3d08046e62c1
Kristjan <kriistjanm@gmail.com>
2021-08-09 14:21:04 +0700

    GPT

--- components/Scripts/GooglePublisherTags.tsx
+++ components/Scripts/GooglePublisherTags.tsx
@@ -8,38 +8,40 @@ export const GooglePublisherTags = () => {
 	// const ads = getAdUnitList();
 
 	useEffect(() => {
-		const { googletag } = window;
-		googletag.cmd.push(function () {
-			// Sidebar 1 -- sizes
-			const mappingA = googletag
-				.sizeMapping()
-				.addSize(
-					[1024, 0],
-					[
-						[336, 280],
+		if (typeof window !== 'undefined') {
+			const { googletag } = window;
+			googletag.cmd.push(function () {
+				// Sidebar 1 -- sizes
+				const mappingA = googletag
+					.sizeMapping()
+					.addSize(
+						[1024, 0],
+						[
+							[336, 280],
+							[300, 250],
+						]
+					)
+					.addSize([0, 0], [])
+					.build();
+
+				// Sidebar 1 -- slot
+				googletag
+					.defineSlot(
+						'/2507246/SAN//stockanalysis//misc//1',
 						[300, 250],
-					]
-				)
-				.addSize([0, 0], [])
-				.build();
+						'div-gpt-ad-1617185412139-0'
+					)
+					.defineSizeMapping(mappingA)
+					.addService(googletag.pubads());
 
-			// Sidebar 1 -- slot
-			googletag
-				.defineSlot(
-					'/2507246/SAN//stockanalysis//misc//1',
-					[300, 250],
-					'div-gpt-ad-1617185412139-0'
-				)
-				.defineSizeMapping(mappingA)
-				.addService(googletag.pubads());
+				googletag.pubads().enableSingleRequest();
+				googletag.enableServices();
+			});
 
-			googletag.pubads().enableSingleRequest();
-			googletag.enableServices();
-		});
-
-		googletag.cmd.push(function () {
-			googletag.display('div-gpt-ad-1617185412139-0');
-		});
+			googletag.cmd.push(function () {
+				googletag.display('div-gpt-ad-1617185412139-0');
+			});
+		}
 	}, []);
 
 	return <></>;
