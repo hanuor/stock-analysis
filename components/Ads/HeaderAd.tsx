38cffeb86f208a66e7e873a4e3a829705d96d9e9
Kristjan <kriistjanm@gmail.com>
2021-08-09 19:03:23 +0700

    GPT

--- components/Ads/HeaderAd.tsx
+++ components/Ads/HeaderAd.tsx
@@ -8,7 +8,7 @@ export const HeaderAd = () => {
 			<div className="hidden md:block lbl mt-2">
 				<div
 					className={`md:h-[90px] md:w-[728px] mx-auto${
-						process.env.NODE_ENV === 'development' && ' bg-gray-100'
+						process.env.NODE_ENV === 'development' ? ' bg-gray-100' : ''
 					}`}
 					id="div-gpt-ad-1617185422059-0"
 				></div>
--- components/Ads/Sidebar1.tsx
+++ components/Ads/Sidebar1.tsx
@@ -7,7 +7,7 @@ export const Sidebar1 = () => {
 		<>
 			<div
 				className={`min-h-[250px] max-h-[280px] min-w-[300px] mx-auto${
-					process.env.NODE_ENV === 'development' && ' bg-gray-100'
+					process.env.NODE_ENV === 'development' ? ' bg-gray-100' : ''
 				}`}
 				id="div-gpt-ad-1617185412139-0"
 			></div>
