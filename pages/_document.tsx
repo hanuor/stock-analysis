d2e7167a7cd0eeded5536cc7282a19d0bcb20437
Kristjan <kriistjanm@gmail.com>
2021-08-10 15:24:10 +0700

    GPT

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,17 +4,7 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head>
-					{/* <script
-						async
-						src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
-					></script>
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `const googletag = (window.googletag = window.googletag || { cmd: [] });`,
-						}}
-					/> */}
-				</Head>
+				<Head />
 				<body>
 					<Main />
 					<NextScript />
