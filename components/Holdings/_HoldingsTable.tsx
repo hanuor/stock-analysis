1974696bcbdbbad9d0b271e3ec61c96a098ac32a
Kris <kriistjanm@gmail.com>
2021-08-13 18:51:11 +0700

    Disable pagination on holdings tables

--- components/Holdings/_HoldingsTable.tsx
+++ components/Holdings/_HoldingsTable.tsx
@@ -8,6 +8,14 @@ import { authState } from 'state/authState';
 
 export const HoldingsTable = ({ rawdata }: { rawdata: Holding[] }) => {
 	const isPro = authState((state) => state.isPro);
+	const count = rawdata.length;
+
+	let showData: Holding[];
+	if (isPro) {
+		showData = rawdata;
+	} else {
+		showData = rawdata.slice(0, 200);
+	}
 
 	type CellString = {
 		cell: {
@@ -49,33 +57,16 @@ export const HoldingsTable = ({ rawdata }: { rawdata: Holding[] }) => {
 		[]
 	);
 
-	const data = useMemo(() => rawdata, [rawdata]);
+	const data = useMemo(() => showData, [showData]);
 
-	const {
-		getTableProps,
-		getTableBodyProps,
-		headerGroups,
-		prepareRow,
-		rows,
-		page,
-		canPreviousPage,
-		canNextPage,
-		pageOptions,
-		nextPage,
-		previousPage,
-		setPageSize,
-		state: { pageIndex, pageSize },
-	} = useTable(
-		{
-			columns,
-			data,
-			initialState: {
-				pageIndex: 0,
-				pageSize: 200,
+	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+		useTable(
+			{
+				columns,
+				data,
 			},
-		},
-		usePagination
-	);
+			usePagination
+		);
 
 	return (
 		<>
@@ -93,7 +84,7 @@ export const HoldingsTable = ({ rawdata }: { rawdata: Holding[] }) => {
 						))}
 					</thead>
 					<tbody {...getTableBodyProps()}>
-						{page.map((row, index) => {
+						{rows.map((row, index) => {
 							prepareRow(row);
 							return (
 								<tr {...row.getRowProps()} key={index}>
@@ -110,45 +101,7 @@ export const HoldingsTable = ({ rawdata }: { rawdata: Holding[] }) => {
 					</tbody>
 				</table>
 			</div>
-			{isPro && rows.length > 200 ? (
-				<nav className="mt-2.5 px-1 flex flex-row justify-between space-x-2 text-sm sm:text-base">
-					<button
-						onClick={() => previousPage()}
-						disabled={!canPreviousPage}
-						className="relative inline-flex items-center px-1 xs:px-1.5 sm:px-4 py-1.5 xs:py-2 whitespace-nowrap border border-gray-300 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-					>
-						{`< Previous`}
-					</button>
-					<div className="flex flex-row items-center space-x-3 xs:space-x-4">
-						<span className="whitespace-nowrap">
-							<span className="hidden xs:inline">Page </span>
-							{`${pageIndex + 1} of ${pageOptions.length}`}
-						</span>
-						<select
-							value={pageSize}
-							onChange={(e) => {
-								setPageSize(Number(e.target.value));
-							}}
-							name="perpage"
-							className="block w-full pl-2 xs:pl-2.5 sm:pl-3 pr-8 xs:pr-9 sm:pr-10 py-1.5 xs:py-2 border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 rounded-md text-sm sm:text-base"
-						>
-							<option value="200">200</option>
-							<option value="500">500</option>
-							<option value="1000">1000</option>
-							<option value="10000">10000</option>
-						</select>
-					</div>
-					<button
-						onClick={() => nextPage()}
-						disabled={!canNextPage}
-						className="relative inline-flex items-center px-1 xs:px-1.5 sm:px-4 py-1.5 xs:py-2 whitespace-nowrap border border-gray-300 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-					>
-						{`Next >`}
-					</button>
-				</nav>
-			) : (
-				<HoldingsPaywall total={rows.length} />
-			)}
+			{!isPro && <HoldingsPaywall total={count} />}
 		</>
 	);
 };
