092f3ccbf1f2de8370bf4c57e213ee942ef95239
azidn1nja <azidspider@hotmail.com>
2021-08-14 04:06:22 +0000

    Removing transpiler

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-const withTM = require('next-transpile-modules')([
-	'd3-format',
-	'd3-array',
-	'd3-time-format',
-	'd3-scale',
-	'd3-color',
-	'd3-interpolate',
-	'd3-time',
-	'internmap',
-	'd3-shape',
-	'd3-path',
-]); // pass the modules you would like to see transpiled
-
-module.exports = withTM({
+module.exports = {
 	trailingSlash: true,
-});
+};
