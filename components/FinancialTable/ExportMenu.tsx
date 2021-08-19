344a0108fd481033859cf60f4717c5d5af514652
Kris <kriistjanm@gmail.com>
2021-08-19 16:48:01 +0700

    Adjusted the paywall message, only show if the financials are being cut off

--- components/FinancialTable/ExportMenu.tsx
+++ components/FinancialTable/ExportMenu.tsx
@@ -7,12 +7,23 @@ const menuBtn = 'shadow-sm py-2 px-3 text-left bg-white hover:bg-gray-100';
 interface Props {
 	statement: string;
 	symbol: string;
+	fullcount: number;
+	showcount: number;
 	setExportOpen: (arg: boolean) => void;
 }
 
-export const ExportMenu = ({ statement, symbol, setExportOpen }: Props) => {
+export const ExportMenu = ({
+	statement,
+	symbol,
+	fullcount,
+	showcount,
+	setExportOpen,
+}: Props) => {
 	const range = financialsState((state) => state.range);
 	const isPro = authState((state) => state.isPro);
+
+	const period = range === 'annual' ? 'years' : 'quarters';
+	const paywallMessage = `* Showing ${showcount} of ${fullcount} ${period}. Export the full financial history with a free trial of Stock Analysis Pro: https://stockanalysis.com/pro/`;
 
 	const exp = (type: 'csv' | 'xls' | 'xlsx') => {
 		setExportOpen(false);
@@ -29,24 +40,25 @@ export const ExportMenu = ({ statement, symbol, setExportOpen }: Props) => {
 					name: symbol.toUpperCase(),
 					from: { table: 'financial-table' },
 					fixArray: (array) => {
-						if (!isPro) {
+						if (!isPro && showcount < fullcount) {
 							if (type == 'csv') {
-								array[array.length - 1].push([
-									'\n\n' +
-										'*Showing 10 of 26 years. Export the full financial history with Stock Analysis Pro: https://stockanalysis.com/pro/',
-								]);
+								array[array.length] = [];
+								array[array.length] = [paywallMessage];
 							} else {
 								array[3].push(['']);
-								array[3].push([
-									'Get unlimited exports with financial history',
-								]);
+								array[3].push(
+									`* Showing ${showcount} of ${fullcount} ${period}.`
+								);
 								array[4].push(['']);
-								array[4].push(['all the way back to 1995']);
 								array[5].push(['']);
-								array[5].push(['with Stock Analysis Pro:']);
+								array[5].push(['Export the full financial history']);
+								array[6].push(['']);
+								array[6].push([
+									'with a free trial of Stock Analysis Pro:',
+								]);
 
-								array[7].push(['']);
-								array[7].push(['https://stockanalysis.com/pro/']);
+								array[8].push(['']);
+								array[8].push(['https://stockanalysis.com/pro/']);
 							}
 						}
 
