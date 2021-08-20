9cd03af63aa6667cfc7e501b6770d2914ce9c76f
Kris <kriistjanm@gmail.com>
2021-08-20 08:31:14 +0700

    Removed console log

--- pages/stocks/[symbol]/chart.tsx
+++ pages/stocks/[symbol]/chart.tsx
@@ -17,7 +17,6 @@ const CandleStickStockChart = ({ info }: ChartProps) => {
 	const [period, setPeriod] = useState<string>('d');
 	const [time, setTime] = useState<string>('1Y');
 	const [type, setType] = useState<string>('candlestick');
-	console.log(period);
 
 	return (
 		<>
