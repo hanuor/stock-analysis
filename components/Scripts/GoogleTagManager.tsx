251e7d45d390c8effdbc154e1ec6d223aca48982
Kris <kriistjanm@gmail.com>
2021-08-24 16:59:13 +0700

    components/Scripts/GoogleTagManager:

--- components/Scripts/GoogleTagManager.tsx
+++ components/Scripts/GoogleTagManager.tsx
@@ -2,11 +2,19 @@ import Script from 'next/script';
 
 export const GoogleTagManager = () => {
 	return (
-		<Script
-			strategy="afterInteractive"
-			dangerouslySetInnerHTML={{
-				__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
-			}}
-		/>
+		<>
+			<Script
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
+				}}
+			/>
+			<Script
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}`,
+				}}
+			/>
+		</>
 	);
 };
