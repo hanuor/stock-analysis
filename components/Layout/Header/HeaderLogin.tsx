38327f6d2df9c22a295fc59e01050e2b32d57b93
Kris <kriistjanm@gmail.com>
2021-08-24 16:03:18 +0700

    Minor fix

--- components/Layout/Header/HeaderLogin.tsx
+++ components/Layout/Header/HeaderLogin.tsx
@@ -16,7 +16,6 @@ export const HeaderLogin = ({ setOpen }: Props) => {
 					<a
 						className="block lg:inline py-2 px-3 flex-1 text-white bg-gray-500 lg:flex-none lg:bg-white lg:text-black lg:font-normal hover:text-blue-700"
 						onClick={() => setOpen(false)}
-						id="free-trial-header"
 					>
 						Log In
 					</a>
@@ -34,6 +33,7 @@ export const HeaderLogin = ({ setOpen }: Props) => {
 					<a
 						className="block lg:inline-block py-2 px-4 lg:py-1 lg:px-3 flex-1 bg-blue-brand_light lg:flex-none text-white lg:rounded-sm lg:font-normal hover:bg-blue-brand_sharp transition duration-200"
 						onClick={() => setOpen(false)}
+						id="free-trial-header"
 					>
 						Free Trial
 					</a>
