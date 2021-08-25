b26183ffdf971d866403115dd182f99c36ee9bc4
Kris <kriistjanm@gmail.com>
2021-08-25 10:59:05 +0700

    Fix redirects for ipos/year pages

--- next.config.js
+++ next.config.js
@@ -3,4 +3,23 @@ module.exports = {
 	experimental: {
 		scrollRestoration: true,
 	},
+	async redirects() {
+		return [
+			{
+				source: '/ipos/2021-list/',
+				destination: '/ipos/2021/',
+				permanent: true,
+			},
+			{
+				source: '/ipos/2020-list/',
+				destination: '/ipos/2020/',
+				permanent: true,
+			},
+			{
+				source: '/ipos/2019-list/',
+				destination: '/ipos/2019/',
+				permanent: true,
+			},
+		];
+	},
 };
