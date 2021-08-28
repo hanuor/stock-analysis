7559d0c290da730effb2c97a9bbdd58bfb5ead05
Kris <kriistjanm@gmail.com>
2021-08-28 16:35:56 +0700

    Add unavailable box for empty financials pages

--- components/Unavailable.tsx
+++ components/Unavailable.tsx
@@ -6,8 +6,8 @@ interface UnavailableI {
 
 export const Unavailable = ({ message, small, classes }: UnavailableI) => (
 	<div
-		className={`h-full flex justify-center items-center text-center p-4 md:p-12 bg-gray-50 border border-gray-200 ${
-			small ? 'text-xl' : 'text-3xl'
+		className={`h-full flex justify-center items-center text-center p-6 md:p-12 bg-gray-50 border border-gray-200 ${
+			small ? 'text-lg md:text-xl' : 'text-xl md:text-2xl'
 		} font-semibold rounded-sm${classes ? ' ' + classes : ''}`}
 	>
 		{message}
