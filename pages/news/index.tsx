cdfbbea623fc568f6e7a5e57f523656106153300
Kris <kriistjanm@gmail.com>
2021-08-31 18:40:44 +0700

    Added social icons to newsletter widget

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -6,6 +6,7 @@ import { NewsNavigation } from 'components/News/NewsNavigation';
 import { Breadcrumbs } from 'components/Breadcrumbs/_Breadcrumbs';
 import { NewsFeed } from 'components/News/_NewsFeed';
 import { NewsWidget } from 'components/News/NewsWidget';
+import { NewsletterWidget } from 'components/Layout/Sidebar/Newsletter';
 
 interface Props {
 	data: News[];
@@ -33,6 +34,7 @@ export const MarketNews = ({ data, other }: Props) => {
 							<NewsFeed data={data} />
 						</div>
 						<aside className="contain sm:uncontain flex flex-col space-y-7 lg:space-y-10 py-6">
+							<NewsletterWidget />
 							<NewsWidget
 								title="Stock News"
 								news={other}
