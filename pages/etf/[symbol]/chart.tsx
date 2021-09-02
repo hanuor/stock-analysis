c8fa77c6667c5ce30ece7069cf385170dc1722c7
Kris <kriistjanm@gmail.com>
2021-09-02 14:49:56 +0700

    Minor css fix

--- pages/etf/[symbol]/chart.tsx
+++ pages/etf/[symbol]/chart.tsx
@@ -31,7 +31,7 @@ const CandleStickStockChart = ({ info }: ChartProps) => {
 			/>
 			<Stock info={info}>
 				<div className="px-2 sm:contain">
-					<div className="">
+					<div className="py-2">
 						<div className="flex flex-row justify-between items-center border border-gray-200 mb-2 text-sm bp:text-base">
 							<Buttons state={time} dispatch={setTime} />
 							<SelectPeriod dispatcher={setPeriod} />
