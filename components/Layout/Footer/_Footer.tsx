10e368c52092301dee690a5728b18a5ffb446b21
Kris <kriistjanm@gmail.com>
2021-09-05 14:55:02 +0700

    Disable pointer events on social icons

--- components/Layout/Footer/_Footer.tsx
+++ components/Layout/Footer/_Footer.tsx
@@ -19,7 +19,7 @@ const navigation = {
 		{ name: 'Terms of Use', href: '/terms-of-use/' },
 		{ name: 'Data Disclaimer', href: '/data-disclaimer/' },
 	],
-}
+};
 
 export const Footer = () => {
 	const { isLoggedIn } = useUserInfo();
@@ -207,7 +207,7 @@ export const Footer = () => {
 								<svg
 									fill="currentColor"
 									viewBox="0 0 24 24"
-									className="h-8 w-8"
+									className="h-8 w-8 pointer-events-none"
 									aria-hidden="true"
 								>
 									<path
@@ -228,7 +228,7 @@ export const Footer = () => {
 								<svg
 									fill="currentColor"
 									viewBox="0 0 24 24"
-									className="h-8 w-8"
+									className="h-8 w-8 pointer-events-none"
 									aria-hidden="true"
 								>
 									<path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -245,7 +245,7 @@ export const Footer = () => {
 								<svg
 									fill="currentColor"
 									viewBox="0 0 24 24"
-									className="h-8 w-8"
+									className="h-8 w-8 pointer-events-none"
 									aria-hidden="true"
 								>
 									<path
