a2259efa49c1315fd1e41bb0440c5a4da60867e5
azidn1nja <azidspider@hotmail.com>
2021-09-14 22:10:19 +0000

    Polishing the look

--- components/Controls/SplitsFilter.tsx
+++ components/Controls/SplitsFilter.tsx
@@ -9,7 +9,7 @@ export function SplitsFilter({ setColumnFilter }: Props) {
 				<select
 					id="period"
 					name="period"
-					className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-3 bp:px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+					className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm bp:px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
 					defaultValue="All Types"
 					onChange={(e) => {
 						setColumnFilter('splitType', e.target.value);
