567edde7b3f53a3f40128f2046631234faa1b5d6
azidn1nja <azidspider@hotmail.com>
2021-09-14 22:19:29 +0000

    Slightly fixing look

--- components/Controls/SplitsFilter.tsx
+++ components/Controls/SplitsFilter.tsx
@@ -9,16 +9,15 @@ export function SplitsFilter({ setColumnFilter }: Props) {
 				<select
 					id="period"
 					name="period"
-					className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm bp:px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+					className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm pl-4 pr-9 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
 					defaultValue="All Types"
 					onChange={(e) => {
 						setColumnFilter('splitType', e.target.value);
-						// setValue(e.target.value);
 					}}
 				>
-					<option value="">All Types</option>
-					<option value="Reverse">Reverse</option>
+					<option value="">Show All</option>
 					<option value="Forward">Forward</option>
+					<option value="Reverse">Reverse</option>
 				</select>
 			</span>
 		</div>
