0ef8a12b71fe8f455691dc94da3f054f79523523
Kris <kriistjanm@gmail.com>
2021-09-14 14:30:54 +0700

    CSS change

--- components/IPOs/CalendarStats.tsx
+++ components/IPOs/CalendarStats.tsx
@@ -14,35 +14,35 @@ export function CalendarStats({ data }: { data: CalendarData }) {
 			</h2>
 			<div className="border px-6 py-4 rounded text-sm font-medium text-gray-600">
 				<div className="flex flex-wrap justify-around lg:justify-between gap-x-6 gap-y-4 text-center">
-					<div className="flex flex-col-reverse">
-						This Week
-						<div className="font-semibold text-4xl text-gray-800">
+					<div className="flex flex-col">
+						<div className="order-2">This Week</div>
+						<div className="order-1 font-semibold text-4xl text-gray-800">
 							{data.thisweek.length}
 						</div>
 					</div>
-					<div className="flex flex-col-reverse">
-						Next Week
-						<div className="font-semibold text-4xl text-gray-800">
+					<div className="flex flex-col">
+						<div className="order-2">Next Week</div>
+						<div className="order-1 font-semibold text-4xl text-gray-800">
 							{data.nextweek.length}
 						</div>
 					</div>
 					{data.later?.length && (
-						<div className="flex flex-col-reverse">
-							Later
-							<div className="font-semibold text-4xl text-gray-800">
+						<div className="flex flex-col">
+							<div className="order-2">Later</div>
+							<div className="order-1 font-semibold text-4xl text-gray-800">
 								{data.later.length}
 							</div>
 						</div>
 					)}
-					<div className="flex flex-col-reverse">
-						Unscheduled
-						<div className="font-semibold text-4xl text-gray-800">
+					<div className="flex flex-col">
+						<div className="order-2">Unscheduled</div>
+						<div className="order-1 font-semibold text-4xl text-gray-800">
 							{data.unknown.length}
 						</div>
 					</div>
-					<div className="flex flex-col-reverse">
-						Total Upcoming
-						<div className="font-semibold text-4xl text-gray-800">
+					<div className="flex flex-col">
+						<div className="order-2">Total Upcoming</div>
+						<div className="order-1 font-semibold text-4xl text-gray-800">
 							{total}
 						</div>
 					</div>
