b957433c4a94c959f1c571674c5fd105d774e970
azidn1nja <azidspider@hotmail.com>
2021-09-21 23:40:24 +0000

    Another one

--- pages/stocks/[symbol]/chart.tsx
+++ pages/stocks/[symbol]/chart.tsx
@@ -32,7 +32,23 @@ const CandleStickStockChart = ({ info }: ChartProps) => {
 		},
 		{ passive: false }
 	); */
+	document.addEventListener('gesturestart', function (e) {
+		e.preventDefault();
+		// special hack to prevent zoom-to-tabs gesture in safari
+		// document.body.style.zoom = 0.99;
+	});
 
+	document.addEventListener('gesturechange', function (e) {
+		e.preventDefault();
+		// special hack to prevent zoom-to-tabs gesture in safari
+		// document.body.style.zoom = 0.99;
+	});
+
+	document.addEventListener('gestureend', function (e) {
+		e.preventDefault();
+		// special hack to prevent zoom-to-tabs gesture in safari
+		// document.body.style.zoom = 0.99;
+	});
 	return (
 		<Stock info={info} url={`/stocks/${info.symbol}/chart/`}>
 			<SEO
