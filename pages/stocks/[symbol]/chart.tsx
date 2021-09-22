412e00ff759469e5da8145cc8e561e999300a2a9
azidn1nja <azidspider@hotmail.com>
2021-09-22 02:37:37 +0000

    lets try that again

--- pages/stocks/[symbol]/chart.tsx
+++ pages/stocks/[symbol]/chart.tsx
@@ -22,14 +22,6 @@ interface ChartProps {
 }
 
 const h1Styles: CSS.Properties = {
-	backgroundColor: 'rgba(255, 255, 255, 0.85)',
-	position: 'absolute',
-	right: 0,
-	bottom: '2rem',
-	padding: '0.5rem',
-	fontFamily: 'sans-serif',
-	fontSize: '1.5rem',
-	boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
 	['touch-action' as any]: 'none',
 };
 
