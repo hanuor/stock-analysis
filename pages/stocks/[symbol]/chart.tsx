f8eba16fe5616965d26fc7ce051dcd36113f2bb1
Kris <kriistjanm@gmail.com>
2021-09-27 16:16:28 +0700

    Cleanup, adjusted chart height on big phones

--- pages/stocks/[symbol]/chart.tsx
+++ pages/stocks/[symbol]/chart.tsx
@@ -9,9 +9,7 @@ import { GetStaticProps, GetStaticPaths } from 'next';
 import { ParsedUrlQuery } from 'querystring';
 import { useState } from 'react';
 import { Unavailable } from 'components/Unavailable';
-import { useEffect } from 'react';
 import dynamic from 'next/dynamic';
-import CSS from 'csstype';
 
 const StockChart = dynamic(() => import('components/Chart/StockChart'), {
 	ssr: false,
@@ -20,51 +18,12 @@ const StockChart = dynamic(() => import('components/Chart/StockChart'), {
 interface ChartProps {
 	info: Info;
 }
-/*
-const tactionnone: CSS.Properties = {
-	['touch-action' as any]: 'none',
-};
 
-const tactionauto: CSS.Properties = {
-	['touch-action' as any]: 'auto',
-};
-*/
 const CandleStickStockChart = ({ info }: ChartProps) => {
 	const [period, setPeriod] = useState<string>('d');
 	const [loading, setLoading] = useState<boolean>(true);
 	const [time, setTime] = useState<string>('1Y');
 	const [type, setType] = useState<string>('candlestick');
-	/*
-	useEffect(() => {
-		document.addEventListener(
-			'touchmove',
-			function (e) {
-				e.preventDefault();
-			},
-			{ passive: false }
-		); 
-
-		
-		document.addEventListener('gesturestart', function (e) {
-			e.preventDefault();
-			// special hack to prevent zoom-to-tabs gesture in safari
-			// document.body.style.zoom = 0.99;
-		});
-
-		document.addEventListener('gesturechange', function (e) {
-			e.preventDefault();
-			// special hack to prevent zoom-to-tabs gesture in safari
-			// document.body.style.zoom = 0.99;
-		});
-
-		document.addEventListener('gestureend', function (e) {
-			e.preventDefault();
-			// special hack to prevent zoom-to-tabs gesture in safari
-			// document.body.style.zoom = 0.99;
-		});
-	}, []);
-
-	*/
 
 	return (
 		<Stock info={info} url={`/stocks/${info.symbol}/chart/`}>
@@ -73,16 +32,16 @@ const CandleStickStockChart = ({ info }: ChartProps) => {
 				description={`Interactive ${info.nameFull} (${info.ticker}) stock chart with full price history, volume, trends and moving averages.`}
 				canonical={`/stocks/${info.symbol}/chart/`}
 			/>
-			<div className="px-2 sm:contain">
+			<div className="px-2.5 sm:contain">
 				<div className="py-2">
 					<div className="flex flex-row justify-between items-center border border-gray-200 mb-2 text-sm bp:text-base">
 						<Buttons state={time} dispatch={setTime} />
 						<SelectPeriod dispatcher={setPeriod} />
 						<SelectType dispatcher={setType} />
 					</div>
-					<div className="touch-none h-[400px] xs:h-[450px] bp:h-[550px] sm:h-[600px]">
+					<div className="touch-none h-[400px] xs:h-[450px] bp:h-[500px] sm:h-[600px]">
 						{info.state !== 'upcomingipo' ? (
-							<div className="touch-auto h-[400px] xs:h-[450px] bp:h-[550px] sm:h-[600px]">
+							<div className="touch-auto h-[400px] xs:h-[450px] bp:h-[500px] sm:h-[600px]">
 								{loading && <Loading />}
 
 								<StockChart
