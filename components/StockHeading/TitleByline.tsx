be21ee73f12f5a05f78902879193091873d96b6f
Kris <kriistjanm@gmail.com>
2021-09-28 09:33:37 +0700

    Fix title byline bug

--- components/StockHeading/TitleByline.tsx
+++ components/StockHeading/TitleByline.tsx
@@ -1,11 +1,12 @@
 import { useQuote } from 'hooks/useQuote';
 import { Info } from 'types/Info';
+import { isTradingHoursClosed } from 'functions/datetime/isTradingHours';
 
 export function TitleByline({ info }: { info: Info }) {
 	const quote = useQuote(info);
 
 	const timing =
-		quote.exchange === 'OTCMKTS' || quote.ext
+		quote.exchange === 'OTCMKTS' || (quote.ext && !isTradingHoursClosed())
 			? 'Delayed Price'
 			: 'IEX Real-Time Price';
 
