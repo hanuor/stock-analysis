6e1f348a9af0d0408ab9e7693d80cc30dec2eb6a
kristjanmar <kriistjanm@gmail.com>
2021-10-13 14:22:41 +0000

    Upload new logo

--- components/Ads/Snigel/Sidebar1.tsx
+++ components/Ads/Snigel/Sidebar1.tsx
@@ -4,11 +4,7 @@ export function Sidebar1() {
 	const status = authState((state) => state.status);
 	const isPro = authState((state) => state.isPro);
 
-	if (
-		!status ||
-		status !== 'completed' ||
-		(status === 'completed' && !isPro)
-	) {
+	if (status !== 'completed' || (status === 'completed' && !isPro)) {
 		return (
 			<div className="min-h-[250px] hidden lg:block">
 				<div id="adngin-sidebar_1-0"></div>
