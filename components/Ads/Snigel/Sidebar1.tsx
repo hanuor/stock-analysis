8eba9ed2797f796f9d14a71d5bd6dbf2415d2772
kristjanmar <kriistjanm@gmail.com>
2021-10-13 11:50:14 +0000

    Update ad

--- components/Ads/Snigel/Sidebar1.tsx
+++ components/Ads/Snigel/Sidebar1.tsx
@@ -4,13 +4,17 @@ export function Sidebar1() {
 	const status = authState((state) => state.status);
 	const isPro = authState((state) => state.isPro);
 
-	if (status === 'completed' && isPro) {
-		return null;
+	if (
+		!status ||
+		status !== 'completed' ||
+		(status === 'completed' && !isPro)
+	) {
+		return (
+			<div className="min-h-[250px] hidden lg:block">
+				<div id="adngin-sidebar_1-0"></div>
+			</div>
+		);
 	}
 
-	return (
-		<div className="min-h-[250px] hidden lg:block">
-			<div id="adngin-sidebar_1-0"></div>
-		</div>
-	);
+	return null;
 }
