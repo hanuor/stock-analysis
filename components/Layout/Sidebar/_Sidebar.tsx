9064b46123e9aa6a1959b0095599188a894bc2e5
kristjanmar <kriistjanm@gmail.com>
2021-10-14 08:26:18 +0000

    Update ads.txt and add sidebar ad to article pages

--- components/Layout/Sidebar/_Sidebar.tsx
+++ components/Layout/Sidebar/_Sidebar.tsx
@@ -1,8 +1,10 @@
 import { NewsletterWidget } from './Newsletter';
+import { Sidebar1 } from 'components/Ads/Snigel/Sidebar1';
 
 export const Sidebar = () => {
 	return (
 		<aside className="space-y-8 pt-4">
+			<Sidebar1 />
 			<NewsletterWidget />
 		</aside>
 	);
