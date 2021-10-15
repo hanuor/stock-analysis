6218ea6560028b580469cd142e27ed5db71f5297
kristjanmar <kriistjanm@gmail.com>
2021-10-15 12:14:45 +0000

    Add change consent button to privacy policy page

--- components/Ads/ChangeConsent.tsx
+++ components/Ads/ChangeConsent.tsx
@@ -1,6 +1,6 @@
 import { authState } from 'state/authState';
 
-// declare const adconsent: any;
+declare const adconsent: any;
 
 export const ChangeConsent = () => {
 	const status = authState((state) => state.status);
@@ -10,31 +10,29 @@ export const ChangeConsent = () => {
 		return null;
 	}
 
-	return null;
+	if (
+		typeof window === 'undefined' ||
+		process.env.NODE_ENV === 'development'
+	) {
+		return null;
+	}
 
-	// if (
-	// 	typeof window === 'undefined' ||
-	// 	process.env.NODE_ENV === 'development'
-	// ) {
-	// 	return null;
-	// }
+	function modifyConsent() {
+		adconsent('showGUI');
+	}
 
-	// function modifyConsent() {
-	// 	adconsent('showGUI');
-	// }
-
-	// return (
-	// 	<>
-	// 		<p>
-	// 			If you want to change your ad consent preferences, click this button
-	// 			to customize your settings:
-	// 		</p>
-	// 		<button
-	// 			className="px-4 py-2 border border-transparent text-lg font-medium rounded-sm shadow-sm text-white bg-blue-brand_light hover:bg-blue-brand_sharp focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mb-5"
-	// 			onClick={() => modifyConsent()}
-	// 		>
-	// 			Change Consent
-	// 		</button>
-	// 	</>
-	// );
+	return (
+		<>
+			<p>
+				If you want to change your ad consent preferences, click this button
+				to customize your settings:
+			</p>
+			<button
+				className="px-4 py-2 border border-transparent text-lg font-medium rounded-sm shadow-sm text-white bg-blue-brand_light hover:bg-blue-brand_sharp focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mb-5"
+				onClick={() => modifyConsent()}
+			>
+				Change Consent
+			</button>
+		</>
+	);
 };
