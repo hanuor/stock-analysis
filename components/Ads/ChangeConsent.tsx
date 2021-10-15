7f447e8446cf532bc275acc3a461aa23e822d2c1
kristjanmar <kriistjanm@gmail.com>
2021-10-15 07:10:03 +0000

    Comment out change consent

--- components/Ads/ChangeConsent.tsx
+++ components/Ads/ChangeConsent.tsx
@@ -1,6 +1,6 @@
 import { authState } from 'state/authState';
 
-declare const adconsent: any;
+// declare const adconsent: any;
 
 export const ChangeConsent = () => {
 	const status = authState((state) => state.status);
@@ -10,29 +10,31 @@ export const ChangeConsent = () => {
 		return null;
 	}
 
-	if (
-		typeof window === 'undefined' ||
-		process.env.NODE_ENV === 'development'
-	) {
-		return null;
-	}
+	return null;
 
-	function modifyConsent() {
-		adconsent('showGUI');
-	}
+	// if (
+	// 	typeof window === 'undefined' ||
+	// 	process.env.NODE_ENV === 'development'
+	// ) {
+	// 	return null;
+	// }
 
-	return (
-		<>
-			<p>
-				If you want to change your ad consent preferences, click this button
-				to customize your settings:
-			</p>
-			<button
-				className="px-4 py-2 border border-transparent text-lg font-medium rounded-sm shadow-sm text-white bg-blue-brand_light hover:bg-blue-brand_sharp focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mb-5"
-				onClick={() => modifyConsent()}
-			>
-				Change Consent
-			</button>
-		</>
-	);
+	// function modifyConsent() {
+	// 	adconsent('showGUI');
+	// }
+
+	// return (
+	// 	<>
+	// 		<p>
+	// 			If you want to change your ad consent preferences, click this button
+	// 			to customize your settings:
+	// 		</p>
+	// 		<button
+	// 			className="px-4 py-2 border border-transparent text-lg font-medium rounded-sm shadow-sm text-white bg-blue-brand_light hover:bg-blue-brand_sharp focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mb-5"
+	// 			onClick={() => modifyConsent()}
+	// 		>
+	// 			Change Consent
+	// 		</button>
+	// 	</>
+	// );
 };
