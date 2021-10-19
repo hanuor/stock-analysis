63067803187de773c8845ae00dc641b234119291
kristjanmar <kriistjanm@gmail.com>
2021-10-19 10:01:41 +0000

    Fix Sidebar1Overview ad

--- components/Ads/Snigel/Sidebar1Overview.tsx
+++ components/Ads/Snigel/Sidebar1Overview.tsx
@@ -8,15 +8,16 @@ export function Sidebar1Overview({ news }: { news: News[] }) {
 	const status = authState((state) => state.status);
 	const isPro = authState((state) => state.isPro);
 
-	const count = news.length || 0;
-	const classes =
-		count < 6
-			? 'block min-h-[265px] mt-8 sm:hidden sm:mt-0 mx-auto'
-			: 'block min-h-[265px] mt-8 sm:hidden lg:block lg:mt-0 lg:min-h-[301px] mx-auto';
-
 	if (!noAds(path.one)) {
 		if (status !== 'completed' || (status === 'completed' && !isPro)) {
-			return <div id="adngin-sidebar_1-0" className={classes}></div>;
+			if (news.length > 5) {
+				return (
+					<div
+						id="adngin-sidebar_1-0"
+						className="hidden lg:block lg:min-h-[301px] mx-auto"
+					></div>
+				);
+			}
 		}
 	}
 
