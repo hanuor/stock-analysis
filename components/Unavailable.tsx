84fd1fe972383f52d9b283827e51bd21cf4537ca
Kris <kriistjanm@gmail.com>
2021-10-30 20:27:15 +0700

    Price Charts: Add unavailable message if no data, also add back in support for chart overrides

--- components/Unavailable.tsx
+++ components/Unavailable.tsx
@@ -1,15 +1,17 @@
-interface UnavailableI {
+interface props {
 	message: string;
 	small?: boolean;
 	classes?: string;
 }
 
-export const Unavailable = ({ message, small, classes }: UnavailableI) => (
-	<div
-		className={`h-full flex justify-center items-center text-center p-6 md:p-12 bg-gray-50 border border-gray-200 ${
-			small ? 'text-lg md:text-xl' : 'text-xl md:text-2xl'
-		} font-semibold rounded-sm${classes ? ' ' + classes : ''}`}
-	>
-		{message}
-	</div>
-);
+export function Unavailable({ message, small, classes }: props) {
+	return (
+		<div
+			className={`h-full flex justify-center items-center text-center p-6 md:p-12 bg-gray-50 border border-gray-200 ${
+				small ? 'text-lg md:text-xl' : 'text-xl md:text-2xl'
+			} font-semibold rounded-sm${classes ? ' ' + classes : ''}`}
+		>
+			{message}
+		</div>
+	);
+}
