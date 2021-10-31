9c30a8de70c146513f09acfbafc4945bf10a68f9
Kris <kriistjanm@gmail.com>
2021-10-31 14:54:23 +0700

    Fix padding for IPO price

--- components/StockQuote/_StockQuote.tsx
+++ components/StockQuote/_StockQuote.tsx
@@ -4,7 +4,11 @@ import { LiveQuote } from './LiveQuote';
 
 export function StockQuote({ info }: { info: Info }) {
 	if (info.state === 'upcomingipo') {
-		return <IPOPrice info={info} />;
+		return (
+			<section className="mb-5">
+				<IPOPrice info={info} />
+			</section>
+		);
 	}
 
 	return <LiveQuote info={info} />;
