4c5ea6e62096139607982db375399791d5465b9e
Kris <kriistjanm@gmail.com>
2021-11-05 13:45:09 +0700

    Sticky ad example

--- components/Ads/Snigel/HeaderAd.tsx
+++ components/Ads/Snigel/HeaderAd.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable @next/next/no-img-element */
 import { authState } from 'state/authState';
 import { noAds } from 'components/Ads/noAds';
 import { useNavState } from 'hooks/useNavState';
@@ -47,7 +50,27 @@ export function HeaderAd() {
 							id="adngin-top_leaderboard-0"
 						>
 							<div className="text-xxs pt-1">ADVERTISEMENT</div>
-							<div className="w-[728px] h-[90px] border border-gray-300 bg-blue-100 mx-auto"></div>
+							<a
+								href="https://www.tradingview.com/?offer_id=10&aff_id=23485&file_id=932"
+								target="_blank"
+								className="mx-auto text-center"
+							>
+								<img
+									src="https://media.go2speed.org/brand/files/tradingview/10/Trial_EN_728x90.png"
+									width="728"
+									height="90"
+									className="mx-auto text-center"
+									// border="0"
+								/>
+							</a>
+							<img
+								src="https://tradingview.go2cloud.org/aff_i?offer_id=10&file_id=932&aff_id=23485"
+								width="0"
+								height="0"
+								// style="position:absolute;visibility:hidden;"
+								// border="0"
+							/>
+							{/* <div className="w-[728px] h-[90px] border border-gray-300 bg-blue-100 mx-auto"></div> */}
 						</div>
 					</div>
 				</div>
